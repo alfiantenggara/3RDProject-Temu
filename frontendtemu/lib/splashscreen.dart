f18e10a36d8cca567c0ef62640a297f99dec10c3
leowdij08 <158173182+leowdij08@users.noreply.github.com>
2025-01-17 00:27:28 +0800

    splashscreen dan onboarding update

--- frontendtemu/lib/splashscreen.dart
+++ frontendtemu/lib/splashscreen.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Timer(const Duration(seconds: 4), () {
+      Navigator.of(context).pushReplacementNamed('/onboarding');
+    });
+
     return Scaffold(
-      body: Center(
-        child: Text('Splash Screen'),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color(0xFF4FC3F7),
+              Color(0xFF4C81B7),
+              Color(0xFF4171B9),
+            ],
+          ),
+        ),
+        child: Center(
+          child: Image.asset(
+            'assets/TEMU.png',
+            width: 200,
+          ),
+        ),
       ),
     );
   }
