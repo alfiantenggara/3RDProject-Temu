2bc9736f312cd7dbedc67367302f2f00a848fae8
leowdij08 <158173182+leowdij08@users.noreply.github.com>
2025-01-20 02:16:55 +0800

    dashboard organisasi

--- frontendtemu/lib/app.dart
+++ frontendtemu/lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontendtemu/dashboardorganisasi.dart';
 import 'onboarding.dart'; // File untuk Onboarding
 import 'splashscreen.dart'; // File untuk Splashscreen
 
@@ -21,6 +22,8 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => const SplashScreen(),
         '/onboarding': (context) => const OnboardingPage(),
+        '/dashboardorganisasi': (context) => DashboardOrganisasi(),
+        
       },
     );
   }
